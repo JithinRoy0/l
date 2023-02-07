b8cb3f5e39ddb849148a2c1c3a7aa221d2fe07d8
Jithin Roy <89648908+JithinRoy0@users.noreply.github.com>
2023-02-07 06:45:26 +0530

    Update Linear regression.R

--- Linear regression.R
+++ Linear regression.R
@@ -4,6 +4,5 @@ x=c(1,2,3,4,5)
 y=c(11,22,33,44,55)
 print(summary(lm(y~x)))
 plot(
-  y,x,col="blue",main="Height and weight 
-regression",abline(lm(x~y)),xlab="kg",ylabs="cm"
+  y,x,col="blue",main="Height and weight regression",abline(lm(x~y)),xlab="kg",ylabs="cm"
 )
