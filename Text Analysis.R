f16f9bd8afd1332a993a64d102920c896e448228
Jithin Roy <89648908+JithinRoy0@users.noreply.github.com>
2023-02-07 07:10:48 +0530

    Update Text Analysis.R

--- Text Analysis.R
+++ Text Analysis.R
@@ -27,8 +27,7 @@ TextDoc
 
 TextDoc = tm_map(TextDoc, removeNumbers)
 
-writeLines(as.character(TextDoc[[1]])) # inspect doc number one
-
+writeLines(as.character(TextDoc[[1]])) 
 
 # Remove punctuation
 
@@ -36,7 +35,6 @@ writeLines(as.character(TextDoc[[1]])) # inspect doc number one
 TextDoc = tm_map(TextDoc, removePunctuation)
 
 writeLines(as.character(TextDoc[[1]])) # inspect doc number one
-
 
 # Strip whitespace
 
@@ -46,9 +44,7 @@ writeLines(as.character(TextDoc[[1]])) # inspect doc number one
 
 writeLines(as.character(TextDoc[[2]])) # inspect doc number one
 
-
 # # Remove english common stopwords
-
 
 TextDoc = tm_map(TextDoc,removeWords, stopwords("english"))
 writeLines(as.character(TextDoc[[1]])) # inspect doc number one
@@ -56,23 +52,18 @@ writeLines(as.character(TextDoc[[1]])) # inspect doc number one
 # Text stemming - which reduces words to their root form
 # the stem of the words eating, eats, eaten is eat.
 
-
 TextDoc = tm_map(TextDoc, stemDocument)
 
 writeLines(as.character(TextDoc[[1]])) # inspect doc number one
-
 
 # Convert the text to lower case
 TextDoc <- tm_map(TextDoc, content_transformer(tolower))
 writeLines(as.character(TextDoc[[1]])) # inspect doc number one
 
-
 # Remove your own stop word
 # specify your custom stopwords as a character vector
 TextDoc <- tm_map(TextDoc, removeWords, c("s", "company", "team")) 
 TextDoc
-
-
 
 # Sort by descearing value of frequency
 dtm_v <- sort(rowSums(dtm_m),decreasing=TRUE)
@@ -85,24 +76,18 @@ barplot(dtm_d[1:5,]$freq, las = 2, names.arg = dtm_d[1:5,]$word,
         col ="lightgreen", main ="Top 5 most frequent words",
         ylab = "Word frequencies")
 
-
 # Build a term-document matrix
 TextDoc_dtm <- TermDocumentMatrix(TextDoc)
 dtm_m <- as.matrix(TextDoc_dtm)
 dtm_m
 
-
-
 # https://www.red-gate.com/simple-talk/databases/sql-server/bi-sql-server/text-mining-and-sentiment-analysis-with-r/
-
-
 
 #generate word cloud
 #A word cloud is one of the most popular ways to 
 #visualize and analyze qualitative data. It’s
 #of text, where the size of each word indicates 
 #its frequency in that body of text. 
-
 
 set.seed(1234)
 wordcloud(words = dtm_d$word, freq = dtm_d$freq, min.freq = 5,
@@ -126,19 +111,12 @@ wordcloud(words = dtm_d$word, freq = dtm_d$freq, min.freq = 5,
 
 #colors – changes word colors going from lowest to highest frequencies
 
-
-
 # Word Association
-
 
 # Find associations 
 findAssocs(TextDoc_dtm, terms = c("shine","place","move"), corlimit = 0.25)		
 
-
-
 # Sentiment Score
-
-
 
 #Replacing "/", "@" and "|" with space
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
@@ -149,6 +127,4 @@ TextDoc <- tm_map(TextDoc, toSpace, "\\|")
 # Convert the text to lower case
 TextDoc <- tm_map(TextDoc, content_transformer(tolower))
 
-
 print(TextDoc)
-
